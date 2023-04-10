0a02eb98e566dfd599f9e3cffb5c9eade961b8e8
Ana Carolina <101834138+ananazario@users.noreply.github.com>
2023-04-10 18:43:42 -0300

    Fazendo a requisicao de PhotoPost

--- src/Api/Api.jsx
+++ src/Api/Api.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import UserPost from './endpoints/UserPost'
 import TokenPost from './endpoints/TokenPost'
+import PhotoPost from './endpoints/PhotoPost'
 
 const Api = () => {
     return (
         <div>
             <h2>UserPost</h2>
             <UserPost />
+            <h2>TokenPost</h2>
             <TokenPost />
+            <h2>PhotoPost</h2>
+            <PhotoPost />
         </div>
     )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Api from './Api/Api'
+import './App.css'
+
 function App() {
 
   return (
     <div>
-
+      <Api />
     </div>
   )
 }
